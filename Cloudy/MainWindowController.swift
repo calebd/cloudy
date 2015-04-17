c5d335a1d22c291299cf5235ca7f17954cdae83c
Caleb Davenport <calebmdavenport@gmail.com>
2015-04-16 21:51:45 -0700

    Add some toolbar items.

---
+++ Cloudy/MainWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  MainWindowController.swift
+//  Cloudy
+//
+//  Created by Caleb Davenport on 4/16/15.
+//  Copyright (c) 2015 Shiny Bits. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        window?.titleVisibility = .Hidden
+    }
+
+}
